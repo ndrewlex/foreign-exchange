fca35846c8ef02e95a6580db53984b46817cc00a
Andrew <andrew.alexander@ioc.sc>
2019-08-13 23:05:44 +0700

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from "react";
+import "./App.css";
+
+const App: FunctionComponent = () => {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+};
+
+export default App;
